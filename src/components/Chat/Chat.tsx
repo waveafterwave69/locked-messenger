329522ca69f881ebf021486fb6449ef97279d501
Michael Shonov <waveafterwave3w2@gmail.com>
2025-06-16 10:46:26 +0300

    complete?:(

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,99 +1,99 @@
-import React, { useEffect, useRef, useState } from 'react'
-import styles from './Chat.module.css'
-import { io } from 'socket.io-client'
-import { useLocation, useNavigate } from 'react-router-dom'
-import type { Message } from '../../types'
-import { useForm, type SubmitHandler } from 'react-hook-form'
-import Messages from '../Messages/Messages'
+// import React, { useEffect, useRef, useState } from 'react'
+// import styles from './Chat.module.css'
+// import { io } from 'socket.io-client'
+// import { useLocation, useNavigate } from 'react-router-dom'
+// import type { Message } from '../../types'
+// import { useForm, type SubmitHandler } from 'react-hook-form'
+// import Messages from '../Messages/Messages'
 
-const socket = io('https://locked-messenger-server.onrender.com')
+// const socket = io('https://locked-messenger-server.onrender.com')
 
-interface MessageFromServer {
-    data: {
-        user: {
-            room?: string
-            user?: string
-            name?: string
-        }
-        message: string
-    }
-}
+// interface MessageFromServer {
+//     data: {
+//         user: {
+//             room?: string
+//             user?: string
+//             name?: string
+//         }
+//         message: string
+//     }
+// }
 
 const Chat: React.FC = () => {
-    const navigate = useNavigate()
-    const { register, handleSubmit, reset } = useForm<Message>()
-    const { search } = useLocation()
-    const [params, setParams] = useState<{
-        room?: string
-        user?: string
-        name?: string
-    }>({})
-    const [messages, setMessages] = useState<MessageFromServer['data'][]>([])
-    const inputRef = useRef<HTMLInputElement>(null)
-    const [users, setUsers] = useState<number>(0)
-    const messagesEndRef = useRef<HTMLDivElement>(null)
+    // const navigate = useNavigate()
+    // const { register, handleSubmit, reset } = useForm<Message>()
+    // const { search } = useLocation()
+    // const [params, setParams] = useState<{
+    //     room?: string
+    //     user?: string
+    //     name?: string
+    // }>({})
+    // const [messages, setMessages] = useState<MessageFromServer['data'][]>([])
+    // const inputRef = useRef<HTMLInputElement>(null)
+    // const [users, setUsers] = useState<number>(0)
+    // const messagesEndRef = useRef<HTMLDivElement>(null)
 
-    const scrollToBottom = () => {
-        messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' })
-    }
+    // const scrollToBottom = () => {
+    //     messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' })
+    // }
 
-    useEffect(() => {
-        const searchParams = Object.fromEntries(new URLSearchParams(search))
-        setParams(searchParams)
-        socket.emit('join', searchParams)
+    // useEffect(() => {
+    //     const searchParams = Object.fromEntries(new URLSearchParams(search))
+    //     setParams(searchParams)
+    //     socket.emit('join', searchParams)
 
-        return () => {
-            socket.off('joinRoom')
-        }
-    }, [search])
+    //     return () => {
+    //         socket.off('joinRoom')
+    //     }
+    // }, [search])
 
-    useEffect(() => {
-        const handleMessage = ({ data }: MessageFromServer) => {
-            setMessages((prevMessages) => [...prevMessages, data])
-            scrollToBottom()
-        }
-        socket.on('message', handleMessage)
+    // useEffect(() => {
+    //     const handleMessage = ({ data }: MessageFromServer) => {
+    //         setMessages((prevMessages) => [...prevMessages, data])
+    //         scrollToBottom()
+    //     }
+    //     socket.on('message', handleMessage)
 
-        return () => {
-            socket.off('message', handleMessage)
-        }
-    }, [])
+    //     return () => {
+    //         socket.off('message', handleMessage)
+    //     }
+    // }, [])
 
-    useEffect(() => {
-        const handleJoinRoom = ({ data }: { data: { users: any[] } }) => {
-            setUsers(data.users.length)
-        }
+    // useEffect(() => {
+    //     const handleJoinRoom = ({ data }: { data: { users: any[] } }) => {
+    //         setUsers(data.users.length)
+    //     }
 
-        socket.on('joinRoom', handleJoinRoom)
+    //     socket.on('joinRoom', handleJoinRoom)
 
-        return () => {
-            socket.off('joinRoom', handleJoinRoom)
-        }
-    }, [])
+    //     return () => {
+    //         socket.off('joinRoom', handleJoinRoom)
+    //     }
+    // }, [])
 
-    useEffect(() => {
-        scrollToBottom()
-    }, [messages])
+    // useEffect(() => {
+    //     scrollToBottom()
+    // }, [messages])
 
-    const onSubmit: SubmitHandler<Message> = (data) => {
-        if (!data.message) return
+    // const onSubmit: SubmitHandler<Message> = (data) => {
+    //     if (!data.message) return
 
-        socket.emit('sendMessage', { message: data.message, params })
-        reset()
-        if (inputRef.current) {
-            inputRef.current.focus()
-        }
-        scrollToBottom()
-    }
+    //     socket.emit('sendMessage', { message: data.message, params })
+    //     reset()
+    //     if (inputRef.current) {
+    //         inputRef.current.focus()
+    //     }
+    //     scrollToBottom()
+    // }
 
-    const leftRoom = () => {
-        socket.emit('leftRoom', { params })
-        navigate('/')
-    }
+    // const leftRoom = () => {
+    //     socket.emit('leftRoom', { params })
+    //     navigate('/')
+    // }
 
     return (
         <>
-            <section className={styles.chat}>
+            {/* <section className={styles.chat}>
                 <div className={styles.emoji__list}></div>
                 <header className={styles.chat__header}>
                     <h2 className={styles.header__title}>
@@ -128,7 +128,8 @@ const Chat: React.FC = () => {
                         Send
                     </button>
                 </form>
-            </section>
+            </section> */}
+            <p>РАБОТАЙ ПОЖАЛУЙСТА</p>
         </>
     )
 }
